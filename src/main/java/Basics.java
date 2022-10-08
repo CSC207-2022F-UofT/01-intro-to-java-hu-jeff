bbce123c61fcc581bf064d9f90253a9d89c11d79
jeffhu <jeff1hu@gmail.com>
2022-10-07 21:08:08 -0400

    complete

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,8 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+
+        int my_variable = 100;
 
 
 
@@ -92,6 +95,7 @@ public class Basics {
          *                the following at each iteration:
          *                Current count: #
          *
+         *
          * e.g. the end result should look like:
          * Current count: 10
          * Current count: 9
@@ -99,6 +103,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--){
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -120,7 +127,6 @@ public class Basics {
      *
      * You may want to use String.split() and String.charAt() methods.
      * See 1.5.2. APIs for their APIs.
-     *
      * For example, calling the below method on the string:
      *    "Idol Long Oolong Vertical Europe University Toyota"
      * should return "ILOVEUT"
@@ -141,6 +147,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] splitted = to_split.split(" ");
+        for (String split: splitted) {
+            ret.append(split.charAt(0));
+        }
+
 
         // Fill in the rest of the body here
 
@@ -164,6 +175,11 @@ public class Basics {
     public static int oddSum(int[] arr) {
         int current_sum = 0;
 
+        for (int i=0; i < arr.length; i++){
+            if (i % 2 == 1) {
+                current_sum += arr[i];
+            }
+        }
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
